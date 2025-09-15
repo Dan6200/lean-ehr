dadc894a334c318d8d21880455dbe296027133b0
Daniel Nyong <danielnyong66@gmail.com>
2025-09-15 11:01:14 +0100

    feat: Update demo data structure and upload script

--- demo-data/upload.sh
+++ demo-data/upload.sh
@@ -4,14 +4,6 @@ firestore-cli set providers/GYRHOME/facilities -b -f facilities/data.json
 
 jq -r --arg providers "GYRHOME" '
   .[] |
-  select(.data.facility_id?) |
- "firestore-cli set providers/\($providers)/facilities/\(.data.facility_id)/patients \(tojson|@sh)"
-' patients/data.json |
-parallel -j 4 '{}' 1>> upload.log 2>> error.log
-
-jq -r --arg providers "GYRHOME" '
-  .[] |
-  select(.data.facility_id?) |
-	"firestore-cli set providers/\($providers)/facilities/\(.data.facility_id)/patients/\(.data.resident_id)/emergency_contacts \(tojson|@sh)"
-' emergency_contacts/data.json |
+  "firestore-cli set providers/\($providers)/facilities/\(.data.residence_id)/residents/\(.data.resident_id) \(.data | tojson | @sh)"
+' residents/data.json |
 parallel -j 4 '{}' 1>> upload.log 2>> error.log
