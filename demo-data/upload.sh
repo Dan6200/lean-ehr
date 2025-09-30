e5919e52b7a71c65da386210192561051b9840a1
Daniel Nyong <danielnyong66@gmail.com>
2025-09-30 06:44:36 +0100

    Fixed resident/data.json file. Updated upload.sh. Renamed residence_id to facility_id

--- demo-data/upload.sh
+++ demo-data/upload.sh
@@ -3,7 +3,7 @@
 firestore-cli set providers/GYRHOME/facilities -b -f facilities/data.json
 
 jq -r --arg providers "GYRHOME" '
-  .[] |
-  "firestore-cli set providers/\($providers)/facilities/\(.data.residence_id)/residents/\(.data.resident_id) \(.data | tojson | @sh)"
+.[] |select(.data.facility_id?)|
+  "firestore-cli set providers/\($providers)/facilities/\(.data.facility_id)/residents \(tojson | @sh)"
 ' residents/data.json |
 parallel -j 4 '{}' 1>> upload.log 2>> error.log
