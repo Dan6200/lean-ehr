ada1b0440a7cabc5473c0176325053df80bbdaf4
Daniel Nyong <danielnyong66@gmail.com>
2025-10-01 12:02:20 +0100

    Flattened structure for cleaner and more efficient querying

--- demo-data/upload.sh
+++ demo-data/upload.sh
@@ -4,6 +4,6 @@ firestore-cli set providers/GYRHOME/facilities -b -f facilities/data.json
 
 jq -r --arg providers "GYRHOME" '
 .[] |select(.data.facility_id?)|
-  "firestore-cli set providers/\($providers)/facilities/\(.data.facility_id)/residents \(tojson | @sh)"
+  "firestore-cli set providers/\($providers)/residents \(tojson | @sh)"
 ' residents/data.json |
 parallel -j 4 '{}' 1>> upload.log 2>> error.log
