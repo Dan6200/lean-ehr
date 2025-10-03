97f5d4c6cf467fb52dd707f2e5545e00ea47a39f
Daniel Nyong <danielnyong66@gmail.com>
2025-10-03 06:51:12 +0100

    Switched to middleware based token verification

--- app/admin/actions/get-encryption-key.ts
+++ app/admin/actions/get-encryption-key.ts
@@ -1,43 +1,22 @@
-'use server'
+import 'server-only'
 
-import { auth } from '@/firebase/server/config' // Firebase Admin SDK auth
-import { IdTokenResult } from 'firebase/auth'
-
-export async function getEncryptionKey(
-  idToken: IdTokenResult,
-): Promise<{ key?: string; error?: string }> {
-  // console.log(idToken)
+export async function getEncryptionKey(): Promise<{
+  key?: string
+  error?: string
+}> {
   try {
-    if (!idToken) {
-      return { error: 'Unauthorized: No ID token provided' }
-    }
-
-    let decodedToken
-    try {
-      decodedToken = await auth.verifyIdToken(idToken?.token)
-    } catch (error) {
-      console.error('Error verifying ID token:', error)
-      return { error: 'Unauthorized: Invalid ID token' }
-    }
-
-    // Optional: Add admin role check here if needed, using decodedToken.uid or custom claims
-    // For example, if you have a custom claim 'admin: true'
-    // if (!decodedToken.admin) {
-    //   return { error: 'Forbidden: Not an admin' };
-    // }
-
     const encryptionKey = process.env.ENCRYPTION_SECRET_KEY
 
     if (!encryptionKey) {
       console.error(
         'ENCRYPTION_SECRET_KEY is not set in environment variables.',
       )
-      return { error: 'Server Error: Encryption key not configured' }
+      throw new Error('Encryption key not configured')
     }
 
     return { key: encryptionKey }
   } catch (error) {
     console.error('Unexpected error in getEncryptionKey Server Action:', error)
-    return { error: 'Internal Server Error' }
+    return { error: 'Internal Server Error: ' + error }
   }
 }
