d1bbf768cbed1162b11263ade91b0fbac122d422
Daniel Nyong <danielnyong66@gmail.com>
2025-10-03 07:38:57 +0100

    Converted back to RSC

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,24 @@
-'use client'
+import { getAllResidentsData } from './admin/residents/actions/get'
+import PaginatedResidentList from '@/components/paginated-resident-list'
 
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
-import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from '@/firebase/client/config'
-import {
-  getAllResidentsData,
-  getAllResidentData,
-} from './admin/residents/actions/get'
-import residentDataList from '@/components/residentData-list'
-import { ResidentData } from '@/types'
+export default async function Home() {
+  const residentsData = await getAllResidentsData().catch((e) => {
+    console.error('Failed to fetch residentData:', e)
+    return null // Handle error gracefully
+  })
 
-export default function Home() {
-  const [residentData, setresidentData] = useState<ResidentData[] | null>(null)
-  const [loading, setLoading] = useState(true)
-  const router = useRouter()
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        // User is logged in, fetch residentData
-        const fetchedresidentData = await getAllResidentsData(
-          user.getIdTokenResult(),
-        ).catch((e) => {
-          console.error('Failed to fetch residentData:', e)
-          return null // Handle error gracefully
-        })
-        setresidentData(fetchedresidentData)
-      } else {
-        // User is not logged in, redirect to sign-in page
-        router.push('/admin/sign-in')
-      }
-      setLoading(false)
-    })
-
-    return () => unsubscribe()
-  }, [router])
-
-  if (loading) {
-    // You can return a loading spinner here
-    return (
-      <main className="sm:container bg-background text-center mx-auto py-32 md:py-24">
-        Loading...
-      </main>
-    )
-  }
-
-  if (!residentData) {
+  if (!residentsData) {
     // This can be a more specific error message or component
     return (
-      <main className="sm:container bg-background text-center mx-auto py-32 md:py-24">
-        Unable to fetch residentData.
+      <main className="sm:container bg-background text-center mx-auto py-48 md:py-32">
+        Error loading residents.
       </main>
     )
   }
 
   return (
-    <main className="sm:container bg-background text-center mx-auto py-32 md:py-24">
-      <ResidentDataList {...{ residentData }} />
+    <main className="sm:container bg-background text-center mx-auto py-48 md:py-32">
+      <PaginatedResidentList {...{ residentsData }} />
     </main>
   )
 }
