bdee189190e4e648c74027a1f12ab7e07ce32e25
Daniel Nyong <danielnyong66@gmail.com>
2025-10-05 22:08:10 +0100

    Modified page into a general layout with the ability to have subpages

---
+++ packages/assisted-living/app/admin/residents/[id]/layout.tsx
@@ -0,0 +1,44 @@
+import { notFound, redirect } from 'next/navigation'
+import { getResidentData } from '@/app/admin/residents/actions/get'
+import { ResidentSchema, Resident as ResidentType } from '@/types'
+import Resident from '@/components/resident'
+import { GoBackLink } from '@/components/go-back-link'
+import { ReactNode } from 'react'
+import { ResidentNav } from '@/components/resident-nav'
+
+export default async function ResidentLayout({
+  children,
+  params: { id },
+}: {
+  children: ReactNode
+  params: { id: string }
+}) {
+  const rawResidentData = await getResidentData(id).catch((e) => {
+    if (e.message.match(/not_found/i)) throw notFound()
+    if (e.message.match(/insufficient permissions/)) redirect('/admin/sign-in')
+    throw new Error(
+      `Unable to pass props to Resident Component -- Tag:22.\n\t${e.message}`,
+    )
+  })
+  let validatedResidentData: ResidentType
+  try {
+    validatedResidentData = ResidentSchema.parse(rawResidentData)
+  } catch (error: any) {
+    throw new Error('Invalid Resident Data -- Tag:31: ' + error.message)
+  }
+
+  return (
+    <main className="bg-background flex flex-col items-start gap-4 container md:px-4 text-center py-24 sm:py-4 lg:py-24 h-fit">
+      <GoBackLink
+        url={`/admin/`}
+        className="cursor-pointer text-blue-500 flex w-full gap-2 sm:gap-5"
+      >
+        Go To Previous Page
+      </GoBackLink>
+      <Resident residentData={validatedResidentData} id={id} />
+      <ResidentNav residentId={id} />
+      <div className="mt-8 w-full">{children}</div>
+    </main>
+  )
+}
+
--- packages/assisted-living/app/admin/residents/[id]/page.tsx
+++ packages/assisted-living/app/admin/residents/[id]/page.tsx
@@ -1,36 +1,3 @@
-import { notFound, redirect } from 'next/navigation'
-import { getResidentData } from '@/app/admin/residents/actions/get'
-import { ResidentSchema, Resident as ResidentType } from '@/types'
-import Resident from '@/components/resident'
-import { GoBackLink } from '@/components/go-back-link'
-
-export default async function ResidentPage({
-  params: { id },
-}: {
-  params: { id: string }
-}) {
-  const rawResidentData = await getResidentData(id).catch((e) => {
-    if (e.message.match(/not_found/i)) throw notFound()
-    if (e.message.match(/insufficient permissions/)) redirect('/admin/sign-in')
-    throw new Error(
-      `Unable to pass props to Resident Component -- Tag:22.\n\t${e}`,
-    )
-  })
-  let validatedResidentData: ResidentType
-  try {
-    validatedResidentData = ResidentSchema.parse(rawResidentData)
-  } catch (error: any) {
-    throw new Error('Invalid Resident Data -- Tag:31: ' + error.message)
-  }
-  return (
-    <main className="bg-background flex flex-col gap-16 container md:px-16 mx-auto text-center py-8 sm:py-16 lg:py-24 h-fit">
-      <GoBackLink
-        url={`/admin/`}
-        className="cursor-pointer text-blue-700 flex w-full gap-2 sm:gap-5"
-      >
-        Go To Previous Page
-      </GoBackLink>
-      <Resident residentData={validatedResidentData} id={id} />
-    </main>
-  )
+export default function ResidentPage() {
+  return null
 }
