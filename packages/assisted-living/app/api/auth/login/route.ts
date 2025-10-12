ed411c963230ce17d651558fb7b17564e8435089
Daniel Nyong <danielnyong66@gmail.com>
2025-10-12 19:03:07 +0100

    debugging SW login

--- packages/assisted-living/app/api/auth/login/route.ts
+++ packages/assisted-living/app/api/auth/login/route.ts
@@ -12,65 +12,65 @@ const authHost = process.env.NEXT_PUBLIC_FIREBASE_AUTH_EMULATOR_HOST!
 export async function POST(request: NextRequest) {
   // 1. Try to get ID token from Authorization header (Service Worker path)
   const authorizationHeader = (await headers()).get('Authorization')
+  console.log('authz header: ', authorizationHeader)
   const authIdToken = authorizationHeader?.split('Bearer ')[1]
-  if (authIdToken) {
-    try {
-      const serverApp = initializeServerApp(firebaseConfig, {
-        authIdToken,
-        releaseOnDeref: headers(),
-      })
+  console.log('auth id token /login', authIdToken)
+  try {
+    const serverApp = initializeServerApp(firebaseConfig, {
+      authIdToken,
+      releaseOnDeref: headers(),
+    })
 
+    process.nextTick(() => {
       const serverAuth = getAuth(serverApp)
       if (process.env.NODE_ENV === 'development')
         try {
           connectAuthEmulator(serverAuth, authHost) // Always throws an error due to race-conditions
         } catch {}
-
-      await serverAuth.authStateReady()
-      return NextResponse.json(
-        { message: 'Session cookie set successfully' },
-        { status: 200 },
-      )
-    } catch (error) {
-      console.error('Failed to initialize FirebaseServerApp: ', error)
-    }
-  }
-
-  try {
-    console.log('using session cookies instead')
-    const { idToken } = await request.json()
-    const cookieStore = await cookies()
-
-    if (!idToken) {
-      return NextResponse.json(
-        { message: 'ID Token required' },
-        { status: 400 },
-      )
-    }
-
-    const response = await fetch(CREATE_SESSION_COOKIE_FUNCTION_URL, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ idToken, expiresIn: SESSION_EXPIRY_MS }),
     })
-    const result = await response.json()
-    if (!response.ok) {
-      throw new Error(result.error || 'Failed to set session cookie')
-    }
-
-    const sessionCookie = result
-    cookieStore.set('__session', sessionCookie, {
-      maxAge: SESSION_EXPIRY_MS / 1000, // Convert MS to seconds
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production', // Use secure in production
-      path: '/',
-      sameSite: 'strict',
-    })
-
     return NextResponse.json(
       { message: 'Session cookie set successfully' },
       { status: 200 },
     )
+  } catch (error) {
+    console.error('Failed to initialize FirebaseServerApp: ', error)
+  }
+
+  try {
+    console.log('using session cookies instead')
+    // const { idToken } = await request.json()
+    // const cookieStore = await cookies()
+    //
+    // if (!idToken) {
+    //   return NextResponse.json(
+    //     { message: 'ID Token required' },
+    //     { status: 400 },
+    //   )
+    // }
+    //
+    // const response = await fetch(CREATE_SESSION_COOKIE_FUNCTION_URL, {
+    //   method: 'POST',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify({ idToken, expiresIn: SESSION_EXPIRY_MS }),
+    // })
+    // const result = await response.json()
+    // if (!response.ok) {
+    //   throw new Error(result.error || 'Failed to set session cookie')
+    // }
+    //
+    // const sessionCookie = result
+    // cookieStore.set('__session', sessionCookie, {
+    //   maxAge: SESSION_EXPIRY_MS / 1000, // Convert MS to seconds
+    //   httpOnly: true,
+    //   secure: process.env.NODE_ENV === 'production', // Use secure in production
+    //   path: '/',
+    //   sameSite: 'strict',
+    // })
+    //
+    // return NextResponse.json(
+    //   { message: 'Session cookie set successfully' },
+    //   { status: 200 },
+    // )
   } catch (error: any) {
     console.error(`Error calling ${CREATE_SESSION_COOKIE_FUNCTION_URL}:`, error)
     return NextResponse.json(
