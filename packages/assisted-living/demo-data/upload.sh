e628be4df68216d47f7068ca99708fe3e8a6de06
Daniel Nyong <danielnyong66@gmail.com>
2025-10-13 18:19:53 +0100

    updated to include prod cmd

--- packages/assisted-living/demo-data/upload.sh
+++ packages/assisted-living/demo-data/upload.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-firestore-cli set providers/GYRHOME/facilities -b -f facilities/data.json
+# Development
+# firestore-cli set providers/GYRHOME/facilities -b -f demo-data/facilities/data.json
+#
+# jq -r --arg providers "GYRHOME" '
+# .[] | "firestore-cli set providers/\($providers)/residents \(tojson | @sh)"
+# ' demo-data/residents/data.json |
+# parallel -j 4 '{}'
+
+# Production
+firestore-cli set providers/GYRHOME/facilities -b -f demo-data/facilities/data.json -k secret-key/assisted-living-app-key.json 
 
 jq -r --arg providers "GYRHOME" '
-.[] | "firestore-cli set providers/\($providers)/residents \(tojson | @sh)"
-' residents/data.json |
+.[] | "firestore-cli set providers/\($providers)/residents -k secret-key/assisted-living-app-key.json \(tojson | @sh)"
+' demo-data/residents/data.json |
 parallel -j 4 '{}'
