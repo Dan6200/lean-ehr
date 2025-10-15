81e4f94f0cbbd5192c2fe74e0d934eabbffe2821
Daniel Nyong <danielnyong66@gmail.com>
2025-10-15 16:30:39 +0100

    updated styling of nav

--- packages/assisted-living/components/resident-nav.tsx
+++ packages/assisted-living/components/resident-nav.tsx
@@ -28,19 +28,24 @@ export function ResidentNav({ residentId }: { residentId: string }) {
         {navLinks.map((link) => {
           const fullHref = `/admin/dashboard/residents/${residentId}${link.href}`
           const isActive = pathname === fullHref
+          console.log('is active', isActive)
+          console.log('full href', fullHref)
+          console.log('pathname', pathname)
           return (
             <NavigationMenuItem key={link.name}>
-              <Link href={fullHref} passHref>
-                <NavigationMenuLink
-                  className={cn(
-                    navigationMenuTriggerStyle(),
-                    'rounded-none hover:border-b-accent-foreground/50 hover:bg-transparent text-muted-foreground hover:text-primary-foreground hover:border-b-2 border-b py-2',
-                  )}
-                  active={isActive}
-                >
+              <NavigationMenuLink
+                className={cn(
+                  navigationMenuTriggerStyle(),
+                  'group',
+                  'rounded-none active:border-b-accent-foreground/50 active:font-bold focus:border-b-accent-foreground/50 focus:font-bold hover:border-b-accent-foreground/40 hover:bg-transparent focus:bg-transparent text-muted-foreground hover:text-primary-foreground/80 active:border-b-2 data-[state=active]:border-b-accent-foreground/50 border-b py-2',
+                )}
+                active={isActive}
+                asChild
+              >
+                <Link href={fullHref} className="">
                   {link.name}
-                </NavigationMenuLink>
-              </Link>
+                </Link>
+              </NavigationMenuLink>
             </NavigationMenuItem>
           )
         })}
