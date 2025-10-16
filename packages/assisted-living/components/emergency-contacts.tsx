290e98d2daba60a8a572afcc775131f932e996d0
Daniel Nyong <danielnyong66@gmail.com>
2025-10-16 01:03:18 +0100

    Fixed layout and styling

--- packages/assisted-living/components/emergency-contacts.tsx
+++ packages/assisted-living/components/emergency-contacts.tsx
@@ -9,44 +9,34 @@ export default function EmergencyContacts({
 }: {
   contacts?: EmergencyContact[] | null
 }) {
-  const numContacts = contacts ? contacts.length : 0
-  let gridColsClass = 'grid-cols-1'
-  if (numContacts >= 2) {
-    gridColsClass = 'grid-cols-1 md:grid-cols-2'
-  }
-
   return (
     <section className="space-y-8">
       <h3 className="text-2xl font-bold">Emergency Contacts</h3>
-      <section
-        className={`grid ${gridColsClass} gap-6 w-full md:w-fit mx-auto`}
-      >
+      <section className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
         {contacts && contacts.length > 0 ? (
           contacts.map((contact, index) => (
             <Link
               key={index + (contact.contact_name?.split(' ')[0] ?? '')}
               href={`tel:${contact.cell_phone}`}
-              className="w-full sm:w-fit"
+              className="w-full"
             >
-              <Card className="justify-between hover:bg-green-700/10 active:bg-green-700/10 flex shadow-md p-4 w-full md:p-6 items-center ">
-                <CardContent className="p-0 flex flex-col gap-2 text-left ">
+              <Card className="h-full hover:bg-green-700/10 active:bg-green-700/10 shadow-md w-full">
+                <CardContent className="p-4 md:p-6 flex flex-col gap-2 text-left">
                   <h3 className="capitalize font-semibold md:text-base">
                     {contact.contact_name}
                   </h3>
                   {contact.relationship && (
                     <p className="capitalize text-sm text-muted-foreground">
-                      {contact.relationship}
+                      {contact.relationship.join(', ')}
                     </p>
                   )}
                   {contact.cell_phone && (
-                    <p className="text-green-700 font-semibold">
+                    <p className="text-green-700 font-semibold flex items-center gap-2 pt-2">
+                      <PhoneCall className="h-4 w-4" />
                       {contact.cell_phone}
                     </p>
                   )}
                 </CardContent>
-                <CardFooter className="shrink p-2">
-                  <PhoneCall className="text-green-700 font-bold mx-auto" />
-                </CardFooter>
               </Card>
             </Link>
           ))
