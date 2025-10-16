30787401230b5e30927e66a7665c723e09e04d5c
Daniel Nyong <danielnyong66@gmail.com>
2025-10-16 01:00:47 +0100

    updated styling; added suspense boundary

--- packages/assisted-living/app/admin/dashboard/residents/[id]/layout.tsx
+++ packages/assisted-living/app/admin/dashboard/residents/[id]/layout.tsx
@@ -1,19 +1,18 @@
 import { notFound, redirect } from 'next/navigation'
 import { getResidentData } from '@/actions/residents/get'
-import { ResidentSchema, Resident as ResidentType } from '@/types'
 import Resident from '@/components/resident'
 import { GoBackLink } from '@/components/go-back-link'
-import { ReactNode } from 'react'
+import { ReactNode, Suspense } from 'react'
 import { ResidentNav } from '@/components/resident-nav'
+import { ResidentLayoutSkeleton } from './loading-skeleton'
 
-export default async function ResidentLayout({
+async function ResidentDataContainer({
+  id,
   children,
-  params,
 }: {
+  id: string
   children: ReactNode
-  params: Promise<{ id: string }>
 }) {
-  const { id } = await params
   const residentData = await getResidentData(id).catch((e) => {
     if (e.message.match(/not_found/i)) throw notFound()
     if (e.message.match(/insufficient permissions/)) redirect('/admin/sign-in')
@@ -23,13 +22,30 @@ export default async function ResidentLayout({
   })
 
   return (
+    <>
+      <Resident residentData={residentData} id={id} />
+      <ResidentNav residentId={id} />
+      <div className="w-full xl:w-2/3 py-16">{children}</div>
+    </>
+  )
+}
+
+export default async function ResidentLayout({
+  children,
+  params,
+}: {
+  children: ReactNode
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await params
+  return (
     <main className="bg-background flex flex-col items-start gap-2 container md:px-4 text-center h-fit">
       <GoBackLink className="cursor-pointer flex w-full gap-2 sm:gap-5">
         Go Back
       </GoBackLink>
-      <Resident residentData={residentData} id={id} />
-      <ResidentNav residentId={id} />
-      <div className="mt-8 w-full">{children}</div>
+      <Suspense fallback={<ResidentLayoutSkeleton />}>
+        <ResidentDataContainer id={id}>{children}</ResidentDataContainer>
+      </Suspense>
     </main>
   )
 }
