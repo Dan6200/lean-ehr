43c5c7631c10f83995ec7fac011c2ec0e91c5226
Daniel Nyong <danielnyong66@gmail.com>
2025-10-18 10:54:06 +0100

    Created add resident page

---
+++ packages/assisted-living/app/admin/dashboard/residents/add/page.tsx
@@ -0,0 +1,6 @@
+import { ResidentFormAdd } from '@/components/residents/form/ResidentFormAdd'
+
+export default function AddResidentPage() {
+  // We can pass a default facility_id if needed, or leave it undefined
+  return <ResidentFormAdd facility_id="" />
+}
