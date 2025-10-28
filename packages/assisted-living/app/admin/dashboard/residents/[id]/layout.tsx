17cc3160e2d2cd5ecc6cc962314191f384a6f46c
Daniel Nyong <danielnyong66@gmail.com>
2025-10-28 14:23:16 +0100

    Better error handling by adding redirect for stale session cookie error

--- packages/assisted-living/app/admin/dashboard/residents/[id]/layout.tsx
+++ packages/assisted-living/app/admin/dashboard/residents/[id]/layout.tsx
@@ -15,7 +15,8 @@ async function ResidentDataContainer({
 }) {
   const residentData = await getResidentData(id).catch((e) => {
     if (e.message.match(/not_found/i)) throw notFound()
-    if (e.message.match(/insufficient permissions/)) redirect('/admin/sign-in')
+    if (e.message.match(/(insufficient permissions|invalid session)/))
+      redirect('/admin/sign-in')
     throw new Error(
       `Unable to pass props to Resident Component -- Tag:22.\n\t${e.message}`,
     )
