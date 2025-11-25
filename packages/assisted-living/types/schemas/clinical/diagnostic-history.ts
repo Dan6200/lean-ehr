dae142b9a345e5801f4eb0f757128f5aec1f67de
Daniel Nyong <danielnyong66@gmail.com>
2025-11-25 17:28:27 +0100

    Removed optional schema, updated coding field

--- packages/assisted-living/types/schemas/clinical/diagnostic-history.ts
+++ packages/assisted-living/types/schemas/clinical/diagnostic-history.ts
@@ -2,17 +2,14 @@ import { z } from 'zod'
 import { ConditionStatusEnum } from '#root/types/enums'
 import { CodeableConceptSchema } from '../codeable-concept'
 
-export const DiagnosticHistorySchema = z
-  .object({
-    id: z.string(),
-    resident_id: z.string(),
-    recorder_id: z.string(),
-    clinical_status: ConditionStatusEnum,
-    recorded_date: z.string(),
-    onset_datetime: z.string(),
-    abatement_datetime: z.string().nullable().optional(),
-    title: z.string(),
-    // code: CodeableConceptSchema, -- for now embed directly...
-    CodeableConceptSchema,
-  })
-  .optional()
+export const DiagnosticHistorySchema = z.object({
+  id: z.string(),
+  resident_id: z.string(),
+  recorder_id: z.string(),
+  clinical_status: ConditionStatusEnum,
+  recorded_date: z.string(),
+  onset_datetime: z.string(),
+  abatement_datetime: z.string().nullable().optional(),
+  title: z.string(),
+  code: CodeableConceptSchema,
+})
