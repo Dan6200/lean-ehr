8a3ca0f8f7674add62109826a600db458fcb848c
Daniel Nyong <danielnyong66@gmail.com>
2025-11-29 17:06:33 +0100

    Fixed incorrect import

--- packages/assisted-living/actions/residents/get/facilities.ts
+++ packages/assisted-living/actions/residents/get/facilities.ts
@@ -2,12 +2,10 @@
 
 import { verifySession } from '#root/auth/server/definitions'
 import { collectionWrapper, getDocsWrapper } from '#root/firebase/admin'
-import {
-  Facility,
-  FacilitySchema,
-} from '#root/types/schemas/administrative/facility'
+import { FacilitySchema } from '#root/types/schemas/administrative/facility'
 import { getFacilityConverter } from '#root/types/converters'
 import { notFound } from 'next/navigation'
+import { Facility } from '#root/types'
 
 export async function getAllFacilities(): Promise<Facility[]> {
   try {
